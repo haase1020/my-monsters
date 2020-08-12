b8cb17a1510261c73c9b780f6b37f46ffcda4282
Jon Haase <haase1020@gmail.com>
2020-08-12 07:36:01 -0400

    create monster cards with styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
 import './App.css';
 
@@ -7,29 +8,20 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'mandi',
-          id: 1,
-        },
-        {
-          name: 'noel',
-          id: 2,
-        },
-        {
-          name: 'timothy',
-          id: 3,
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
