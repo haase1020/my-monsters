50b7d300a70cc5d526c52a389ab81fd30a242a9c
Jon Haase <haase1020@gmail.com>
2020-08-17 05:21:10 -0400

    Updates

---
+++ precache-manifest.460077f282ea6a1363314454a3b3fcc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc68c231c178ee4af81844a5f70dfa3f",
+    "url": "/my-monsters/index.html"
+  },
+  {
+    "revision": "90d26ba9e91b5b5869d0",
+    "url": "/my-monsters/static/css/main.5f17283e.chunk.css"
+  },
+  {
+    "revision": "595c844207d4ef689669",
+    "url": "/my-monsters/static/js/2.4fc7029c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-monsters/static/js/2.4fc7029c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90d26ba9e91b5b5869d0",
+    "url": "/my-monsters/static/js/main.19ed6b57.chunk.js"
+  },
+  {
+    "revision": "a58f716b13efb9d6e822",
+    "url": "/my-monsters/static/js/runtime-main.65742cd4.js"
+  }
+]);
